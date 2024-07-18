0e0a5996644b5c9e9b6265972410fd1ff92312b4
Yuri <yurimfs30@gmail.com>
2024-07-18 16:17:46 -0300

    Implementing: route to register users and get a list of products

--- Web/app.py
+++ Web/app.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from flask_cors import CORS
+from flask_login import UserMixin
 
 # definindo a instância do Flask
 app = Flask(__name__)
@@ -9,8 +11,16 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 # Definindo a instância do objeto SQLAlchemy
 db = SQLAlchemy(app)
+CORS(app)
 
 # Modelagem
+# User (id, username, password)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=True)
+
+
 # Produto (id, name, price, description)
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,9 +35,20 @@ with app.app_context():
     # executa as alterações acumuladas na sessão e aplicá-las ao banco de dados real
     db.session.commit()
 
+@app.route('/register_user', methods=['POST'])
+def register_user():
+    data = request.json
+    if 'username' in data and 'password' in data:
+        user = User(username=data['username'], password=data['password'])
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'message': 'User added successfully'})
+    return jsonify({'message': 'Invalid user data'}), 400
+
+
 # Definir rota de adição de produto
 @app.route('/api/products/add', methods=['POST'])
-def adicionarProduto():
+def add_product():
     data = request.json
     if 'name' in data and 'price' in data:
         product = Product(name=data['name'], price=data['price'],description=data.get('description', ''))
@@ -37,7 +58,7 @@ def adicionarProduto():
     return jsonify({'message': 'Invalid product data'}), 400
 
 @app.route('/api/products/delete/<int:product_id>', methods=['DELETE'])
-def deleteProduto(product_id):
+def delete_produto(product_id):
     product = Product.query.get(product_id)
     if product:
         db.session.delete(product)
@@ -46,7 +67,7 @@ def deleteProduto(product_id):
     return jsonify({'message': 'Product Not Found'}), 404
 
 @app.route('/api/products/<int:product_id>', methods=['GET'])
-def getProductDetails(product_id):
+def get_product_details(product_id):
     product = Product.query.get(product_id)
     if product:
         return jsonify({
@@ -58,7 +79,7 @@ def getProductDetails(product_id):
     return jsonify({'message': 'Product Not Found'}), 404
 
 @app.route('/api/products/update/<int:product_id>', methods=['PUT'])
-def updateProduct(product_id):
+def update_product(product_id):
     product = Product.query.get(product_id)
     if not product:
         return jsonify({'message': 'Product Not Found'}), 404
@@ -72,6 +93,20 @@ def updateProduct(product_id):
         product.description = data['description']
     db.session.commit()
     return jsonify({'message': 'Product Updated Successfully'}), 200
+
+@app.route('/api/products', methods=['GET'])
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        product_data = {
+            'id': product.id,
+            'name': product.name,
+            'price': product.price
+        }
+        product_list.append(product_data)
+        
+    return jsonify(product_list)
 
 # Definir uma rota raiz (pag inicial) e a função que será executada ao requisitar
 @app.route('/')
